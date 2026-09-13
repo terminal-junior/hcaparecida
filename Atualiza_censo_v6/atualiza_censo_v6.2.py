ffede626e3906d2da1b674d5925f246eebbffdc8
Junior <jj.josijunior@gmail.com>
2026-09-13 01:09:23 +0000

    Change window title regex for application connection

--- Atualiza_censo_v6/atualiza_censo_v6.2.py
+++ Atualiza_censo_v6/atualiza_censo_v6.2.py
@@ -124,8 +124,8 @@ while tentativa < MAX_TENTATIVAS:
                 send_keys(f"{fathos_username}{{TAB}}{fathos_password}{{ENTER}}")
                 logging.info("Login feito com sucesso!")
                 time.sleep(1)
-                app = Application(backend="uia").connect(title_re="Fathos - SOCIEDADE BENEFICENTE SANTA TEREZINHA.*", timeout=1)
-                main_window = app.window(title_re="Fathos - SOCIEDADE BENEFICENTE SANTA TEREZINHA.*")
+                app = Application(backend="uia").connect(title_re="teste.*", timeout=1)
+                main_window = app.window(title_re="teste.*")
                 main_window.wait("enabled visible ready", timeout=1)
 
                 relatorios_menu = main_window.child_window(title="Relatórios", control_type="MenuItem")
@@ -140,7 +140,7 @@ while tentativa < MAX_TENTATIVAS:
                 time.sleep(4)
                 logging.info("Relatório do Censo gerado com sucesso.")
 
-                janela = app.window(title_re="Fathos - SOCIEDADE BENEFICENTE SANTA TEREZINHA.*")
+                janela = app.window(title_re="teste.*")
                 botoes = janela.descendants(control_type="Button")
                 botoes[0].invoke()
 
